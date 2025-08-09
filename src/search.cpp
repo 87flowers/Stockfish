d9ce710dcf61d09dd4fe994c31f38de85d17f259
87 <dev@87flowers.com>
2025-08-09 20:38:01 +0100

    Take 4
    Bench: 3870766

--- src/search.cpp
+++ src/search.cpp
@@ -914,7 +914,7 @@ Value Search::Worker::search(
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
         MovePicker mp(pos, ttData.move, probCutBeta - ss->staticEval, &captureHistory);
-        Depth      dynamicReduction = std::max((ss->staticEval - beta) / 300, -3);
+        Depth      dynamicReduction = std::clamp((ss->staticEval - beta) / 300, -3, 4);
         Depth      probCutDepth     = std::max(depth - 5 - dynamicReduction, 0);
 
         while ((move = mp.next_move()) != Move::none())
