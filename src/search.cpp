30574f883a3f4de03c0285c0ac5278cb7623f92e
87 <dev@87flowers.com>
2025-08-25 20:06:00 +0100

    Take 1
    Bench: 2573177

--- src/search.cpp
+++ src/search.cpp
@@ -1251,6 +1251,9 @@ moves_loop:  // When in check, search starts here
             if (!ttData.move)
                 r += 1118;
 
+            if (!is_decisive(ttData.value) && is_valid(ss->staticEval) && is_valid(ttData.value))
+                r -= std::abs(ttData.value - ss->staticEval);
+
             // Note that if expected reduction is high, we reduce search depth here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
                                    newDepth - (r > 3212) - (r > 4784 && newDepth > 2), !cutNode);
