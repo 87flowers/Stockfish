8e1e6c6086f6bb901b203a40020aa11642973f9c
87 <dev@87flowers.com>
2025-08-30 22:32:25 +0100

    Take 1
    Bench: 2227673

--- src/search.cpp
+++ src/search.cpp
@@ -1246,6 +1246,8 @@ moves_loop:  // When in check, search starts here
             // Increase reduction if ttMove is not present
             if (!ttData.move)
                 r += 1118;
+            else
+                r -= ttMoveHistory / 8;
 
             // Note that if expected reduction is high, we reduce search depth here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
