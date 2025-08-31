c42bea6ef59cb90878662e6c152f69d64d4c53f4
87 <dev@87flowers.com>
2025-08-31 18:05:12 +0100

    Take 5
    Bench: 2226244

--- src/search.cpp
+++ src/search.cpp
@@ -962,7 +962,7 @@ moves_loop:  // When in check, search starts here
     }();
     if ((ttData.bound & BOUND_LOWER) && ttData.depth > 0 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
-        return ttData.value;
+        return probCutBeta;
 
     const PieceToHistory* contHist[] = {
       (ss - 1)->continuationHistory, (ss - 2)->continuationHistory, (ss - 3)->continuationHistory,
