3c9e8ef4f05a4996b9c5f26ab877916793ad02ae
87 <dev@87flowers.com>
2025-09-20 02:48:07 +0100

    Take 1
    Bench: 2548610

--- src/search.cpp
+++ src/search.cpp
@@ -882,7 +882,7 @@ Value Search::Worker::search(
         if (nullValue >= beta && !is_win(nullValue))
         {
             if (nmpMinPly || depth < 16)
-                return nullValue;
+                return (3 * nullValue + beta) / 4;
 
             assert(!nmpMinPly);  // Recursive verification is not allowed
 
@@ -895,7 +895,7 @@ Value Search::Worker::search(
             nmpMinPly = 0;
 
             if (v >= beta)
-                return nullValue;
+                return (2 * nullValue + v + beta) / 4;
         }
     }
 
